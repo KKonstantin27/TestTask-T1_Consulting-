48835efd4219d5fb0a778305db38267ed6ea683e
Kim Konstantin <k.konstantin.27.21@gmail.com>
2024-01-30 17:11:49 +1000

    add @Size validation check to StringQueryDTO

--- src/main/java/com/example/TestTask/dtos/StringQueryDTO.java
+++ src/main/java/com/example/TestTask/dtos/StringQueryDTO.java
@@ -1,6 +1,7 @@
 package com.example.TestTask.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class StringQueryDTO {
     @NotEmpty(message = "String query should not be empty")
+    @Size(min = 1, max = 50, message = "String query should be between 1 and 50 characters")
     private String stringQuery;
 }
